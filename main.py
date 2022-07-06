555f683768b9c09206c94ba25751e4a3eae2d708
temuri <temuri.isabekovi@hoyer-group.com>
2022-07-06 11:27:29 +0200

    auf arbeit noch bisschen/ui ist done (bisschen)

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ import kivymd
 from kivymd.app import MDApp
 from kivy.uix.floatlayout import FloatLayout
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+from kivymd.uix.screen import MDScreen
 
 Window.size =[390,844]
 
+class main_screen(MDScreen):
+    pass
 
+class search_screen(MDScreen):
+    pass
 
 class myApp(MDApp):
     def build(self):
-        return FloatLayout()
+        sm = ScreenManager()
+        sm.add_widget(main_screen(name='main_s'))
+        sm.add_widget(search_screen(name='search_s'))
+        return sm
  
 if __name__ == '__main__':
     myApp().run()
